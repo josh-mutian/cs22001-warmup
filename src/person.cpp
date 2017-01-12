7b76c33239631429d78ed01ceddf36b02d9a0ad1
marlonliu <liuwenxuan@uchicago.edu>
2017-01-11 20:45:21 -0600

    finished person.cpp

--- src/person.cpp
+++ src/person.cpp
@@ -1,9 +1,11 @@
 #include "person.h"
 
-bool str_isalpha(const string str){
-    for(int i = 0; i < str.size(); i++)
-    	if((isalpha(str[i]) == 0) || (str[i] == ' '))
-    		return false;
+bool str_isalpha(const string str)
+{
+    int size = str.size();
+    for(int i = 0; i < size; i++)
+        if((isalpha(str[i]) == 0) || (str[i] == ' '))
+            return false;
     return true;
 }
 
@@ -14,121 +16,144 @@ bool str_isalnum(const string s)
     return !s.empty() && it == s.end();
 }
 
-Person::Person() 
-  : username(""), firstname(""), lastname(""), gender(""), age(0), tagline("") {
+Person::Person()
+    : username(""), firstname(""), lastname(""), gender(""), age(0), tagline("")
+{
 }
 
-Person::Person(string _username, string _firstname, string _lastname, 
-               string _gender, int _age, string _tagline) 
-  : username(_username), firstname(_firstname), lastname(_lastname),
-    gender(_gender), age(_age), tagline(_tagline) {
+Person::Person(string _username, string _firstname, string _lastname,
+               string _gender, int _age, string _tagline)
+    : username(_username), firstname(_firstname), lastname(_lastname),
+      gender(_gender), age(_age), tagline(_tagline)
+{
 }
 
-string Person::get_username() {
+string Person::get_username()
+{
     return username;
 }
-string Person::get_firstname() {
+string Person::get_firstname()
+{
     return firstname;
 }
-string Person::get_lastname() {
+string Person::get_lastname()
+{
     return lastname;
 }
-string Person::get_gender(){
+string Person::get_gender()
+{
     return gender;
 }
-int Person::get_age() {
+int Person::get_age()
+{
     return age;
 }
-string Person::get_tagline() {
+string Person::get_tagline()
+{
     return tagline;
 }
-string Person::get_info() {
-	string ret = "";
+string Person::get_info()
+{
+    string ret = "";
     return ret;
 }
 
-bool Person::set_username(string _username) {
-    // TODO
-	// write the function for set_username
-	return false;
+bool Person::set_username(string _username)
+{
+    if (str_isalnum(_username) &&
+            _username.length() <= 64 &&
+            _username.length() > 0 &&
+            isalpha(_username[0])) {
+        username = _username;
+        return true;
+    } else {
+        return false;
+    }
 }
 
-bool Person::set_firstname(string _firstname) {
-    // TODO
-	if (0/* fill in any necessary tests here */) {
+bool Person::set_firstname(string _firstname)
+{
+    if (str_isalpha(_firstname) && _firstname.length() <= 64) {
         firstname = _firstname;
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
 
 
-bool Person::set_lastname(string _lastname) {
-    // TODO
-	if (0/* fill in any necessary tests here */) {
+bool Person::set_lastname(string _lastname)
+{
+    if (str_isalpha(_lastname) && _lastname.length() <= 64) {
         lastname = _lastname;
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
 
-bool Person::set_gender(string _gender){
-    //TODO
-    return false;
+bool Person::set_gender(string _gender)
+{
+    if (_gender == "m" || _gender == "f") {
+        gender = _gender;
+        return true;
+    } else {
+        return false;
+    }
 }
 
-bool Person::set_age(int _age) {
-	// TODO
-    if (0/* fill in any necessary tests here */) {
+bool Person::set_age(int _age)
+{
+    if (_age >= 0 && _age < 128) {
         age = _age;
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
-bool Person::set_tagline(string _tagline) {
-	// TODO
-    if (0/* fill in any necessary tests here */) {
+bool Person::set_tagline(string _tagline)
+{
+    if (_tagline.length() <= 512) {
         tagline = _tagline;
         return true;
-    }
-    else {
+    } else {
         return false;
     }
 }
 
 
 bool Person::set_info(string _username, string _firstname, string _lastname,
-                      int _age, string _tagline) {
-    // TODO
-	// set the information for
-	// 1) username
-	// 2) firstname
-	// 3) lastnam3
-	// 4) age
-	// 5) tagline
-	return false;
+                      int _age, string _tagline)
+{
+    if (set_username(_username) &&
+            set_firstname(_firstname) &&
+            set_lastname(_lastname) &&
+            set_age(_age) &&
+            set_tagline(_tagline)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-void Person::send_msg(Person &recipient, string msg) {
-	// TODO
-	// send message
+void Person::send_msg(Person &recipient, string msg)
+{
+    recipient.get_msg(msg);
+}
+
+void Person::get_msg(string msg)
+{
+    inbox.push(msg);
+}
+
+bool Person::read_msg()
+{
+    if (inbox.size() > 0) {
+        cout << inbox.front();
+        inbox.pop();
+        return true;
+    } else {
+        return false;
     }
-
-void Person::get_msg(string msg) {
-	// TODO
-	// get message
-    }
-
-bool Person::read_msg() {
-	// TODO
-	// print the message if there any message inbox
-    return false;
-
 }
 
