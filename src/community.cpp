521ca42313a3af677033efebd81126ca636c9136
marlonliu <liuwenxuan@uchicago.edu>
2017-01-25 23:16:11 -0600

    finish Community comments

--- src/community.cpp
+++ src/community.cpp
@@ -1,20 +1,46 @@
 #include "community.h"
 
+/**
+ * null Community constructor
+ * 
+ * Constructs a Community instance with empty name and empty people collection
+ *
+ */
 Community::Community()
     : name(""), people(map<string,Person>())
 {
 }
 
+/**
+ * Person constructor
+ * 
+ * @param _name - initial name to be set for the constructed Community
+ * @param _people - initial people collection to be set for the constructed Community
+ *
+ */
 Community::Community(string _name, map<string,Person> _people)
     : name(_name), people(_people)
 {
 }
 
+/**
+ * getter method for name property
+ * 
+ * @return - retreived name value
+ *
+ */
 string Community::get_name()
 {
     return name;
 }
 
+/**
+ * setter method for name property
+ * 
+ * @param _name - the new name to be set
+ * @return - true if the new name is valid and set successfully; false otherwise
+ *
+ */
 bool Community::set_name(string _name)
 {
     if (str_isalnum(_name) &&
@@ -28,6 +54,14 @@ bool Community::set_name(string _name)
     }
 }
 
+/**
+ * add a Person to this Community
+ * 
+ * @param _person - the Person to be added
+ * @return - true if _person is not yet a member of this Community and adding is
+             successful; false otherwise
+ *
+ */
 bool Community::add_person(Person _person)
 {
     const string username = _person.get_username();
@@ -39,7 +73,13 @@ bool Community::add_person(Person _person)
     }
 }
 
-// return the person object for a given username
+/**
+ * find a member by username
+ * 
+ * @param username - username of the target member
+ * @return - target member; return null Person if target username is not found
+ *
+ */
 Person Community::get_member(string username)
 {
     if (people.find(username) != people.end()) {
@@ -49,6 +89,12 @@ Person Community::get_member(string username)
     }
 }
 
+/**
+ * return a list of the usernames of all members
+ * 
+ * @return - the resulting list of usernames
+ *
+ */
 list<string> Community::get_all_usernames()
 {
     list<string> usernames;
@@ -59,6 +105,12 @@ list<string> Community::get_all_usernames()
     return usernames;
 }
 
+/**
+ * print out the usernames of all members
+ * 
+ * @return - void
+ *
+ */
 void Community::print_all_usernames()
 {
     list<string> usernames = get_all_usernames();
@@ -68,10 +120,17 @@ void Community::print_all_usernames()
     }
 }
 
+/**
+ * find members by first name
+ * 
+ * @param firstname - target first name
+ * @return - a list of all Person's having the target first name
+ *
+ */
 list<Person> Community::find_member(string firstname)
 {
     list<Person> ret;
-    for (map<string,Person>::iterator it= people.begin(); 
+    for (map<string,Person>::iterator it = people.begin(); 
          it != people.end(); ++it) {
         Person p = it->second;
         if (p.get_firstname() == firstname) {
@@ -81,10 +140,18 @@ list<Person> Community::find_member(string firstname)
     return ret;
 }
 
+/**
+ * find members by age range
+ * 
+ * @param age_lb - age lower bound
+ * @param age_ub - age upper bound
+ * @return - a list of all Person's within the target age range
+ *
+ */
 list<Person> Community::find_member(int age_lb, int age_ub)
 {
     list<Person> ret;
-    for (map<string,Person>::iterator it= people.begin(); 
+    for (map<string,Person>::iterator it = people.begin(); 
          it != people.end(); ++it) {
         Person p = it->second;
         int age = p.get_age();
@@ -95,6 +162,14 @@ list<Person> Community::find_member(int age_lb, int age_ub)
     return ret;
 }
 
+/**
+ * send message to a set of members
+ * 
+ * @param usernames - a list of the usernames of the target members
+ * @param msg - the message
+ * @return - false if any of the target members is not found; true otherwise
+ *
+ */
 bool Community::send_msg(list<string> usernames, string msg)
 {
     bool ret = true;
